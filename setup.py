d14d28189382bc0554f3c03d73eee96dda2428de
davidfeeney <david@sd3d.com>
2016-11-14 18:43:39 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ plugin_author = "David Feeney"
 plugin_author_email = "david@sd3d.com"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "www.sd3d.com"
+plugin_url = "http://sd3d.com/"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
