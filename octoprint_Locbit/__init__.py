d61306a236def1c632027905df431f98b0b4f28e
Steven Kane <skane@unconfinedsoftware.com>
2017-04-10 13:01:11 -0700

    added --no-cache-dir to pip install command

--- octoprint_Locbit/__init__.py
+++ octoprint_Locbit/__init__.py
@@ -582,7 +582,7 @@ class LocbitPlugin(octoprint.plugin.StartupPlugin,
 
                 commands = ['/usr/bin/apt-get update',
                             '/usr/bin/apt-get install -y ipython python-opencv python-scipy python-numpy python-setuptools python-pip python-pygame python-zbar',
-                            '/usr/local/bin/pip install svgwrite https://github.com/sightmachine/SimpleCV/zipball/master timeout-decorator',
+                            '/usr/local/bin/pip --no-cache-dir install svgwrite https://github.com/sightmachine/SimpleCV/zipball/master timeout-decorator',
                             '/bin/chmod +x /home/pi/oprint/lib/python2.7/site-packages/octoprint_Locbit/qr.py']
 
                 for command in commands:
