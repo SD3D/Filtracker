bafb5fb8875f668f12ae31acbd1e0fbbd8a74e71
Steven Kane <skane@unconfinedsoftware.com>
2017-04-10 01:23:32 -0700

    moved bash commands into python module

--- octoprint_Locbit/__init__.py
+++ octoprint_Locbit/__init__.py
@@ -579,7 +579,14 @@ class LocbitPlugin(octoprint.plugin.StartupPlugin,
                 import subprocess
                 settings().set(['folder', 'slicingProfiles'], '/home/pi/.octoprint/slicingProfiles')
                 settings().set(['slicing', 'defaultSlicer'], 'cura', force=True)
-                subprocess.check_call("sudo /bin/bash /home/pi/oprint/lib/python2.7/site-packages/octoprint_Locbit/install_additional.sh", shell=True)                   
+
+                commands = ['/usr/bin/apt-get update',
+                            '/usr/bin/apt-get install -y ipython python-opencv python-scipy python-numpy python-setuptools python-pip python-pygame python-zbar',
+                            '/usr/local/bin/pip install svgwrite https://github.com/sightmachine/SimpleCV/zipball/master timeout-decorator',
+                            '/bin/chmod +x /home/pi/oprint/lib/python2.7/site-packages/octoprint_Locbit/qr.py']
+
+                for command in commands:
+                        subprocess.check_call("/bin/bash -c 'sudo {}'".format(command), shell=True)
 
 	def on_after_startup(self):
 		self._logger.info("Hello world! I am: %s" % self._settings.get(["did"]))
