ef1a631ee5215d2de0630ae820a48d1d117a805b
Steven Kane <skane@unconfinedsoftware.com>
2017-04-10 18:37:57 -0700

    fixed typo on pip upgrade

--- octoprint_Locbit/__init__.py
+++ octoprint_Locbit/__init__.py
@@ -582,7 +582,7 @@ class LocbitPlugin(octoprint.plugin.StartupPlugin,
 
                 commands = ['/usr/bin/apt-get update',
                             '/usr/bin/apt-get install -y ipython python-opencv python-scipy python-numpy python-setuptools python-pip python-pygame python-zbar',
-                            '/bin/chmod +x /home/pi/oprint/lib/python2.7/site-packages/octoprint_Locbit/qr.py'
+                            '/bin/chmod +x /home/pi/oprint/lib/python2.7/site-packages/octoprint_Locbit/qr.py',
                             '/usr/bin/pip install --upgrade pip',
                             '/usr/local/bin/pip --no-cache-dir install timeout-decorator svgwrite https://github.com/sightmachine/SimpleCV/zipball/master'
                            ]
