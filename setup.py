72d9de74106f6d8748301f93fad7252e9947b3a5
davidfeeney <david@sd3d.com>
2017-05-09 22:08:46 -0500

    Update setup.py
    Revert

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@
 ### Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "Filtracker"
+plugin_identifier = "SD3D"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
-plugin_package = "octoprint_Filtracker"
+plugin_package = "octoprint_SD3D"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "OctoPrint-Filtracker"
+plugin_name = "OctoPrint-SD3D"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
 plugin_version = "0.1.0"
